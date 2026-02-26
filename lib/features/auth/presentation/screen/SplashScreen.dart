b87493a135833d3055d2c67a281d2855b82e8700
Alaa Ahmed Radwan <alaaradwan95.ar@gmail.com>
2026-02-26 20:36:27 +0200

    Initial commit: Project Structure, Core, and Assets

---
+++ lib/features/auth/presentation/screen/SplashScreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/onboarding');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 121.w,
+          height: 118.h,
+        ),
+      ),
+    );
+  }
+}
